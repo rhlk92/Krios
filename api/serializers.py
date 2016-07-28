d9b7964f6de0b0770549c73dd9fb7cb45701f7b0
Rahul Karkera <rhlk92@gmail.com>
2016-07-28 13:49:56 +0530

    some changes

--- api/serializers.py
+++ api/serializers.py
@@ -5,4 +5,4 @@ class TaskSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Task
-        fields = ('title', 'description')
+        fields = ('description', 'completed', 'updated')
